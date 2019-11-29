a2f93ef5ecef2656f4520d39665a357f7ff772b8
frederr45 <frederr.45@gmail.com>
2019-11-29 22:30:53 +0300

    first commit

--- work_2.py
+++ work_2.py
@@ -1,4 +1,3 @@
-import pprint
 import re
 import time
 
@@ -7,6 +6,7 @@ from bs4 import BeautifulSoup
 
 posts = []
 dict_ = {}
+
 
 def get_html(url):
     try:
@@ -27,26 +27,30 @@ def get_link():
 def page(link):
     page = get_html(link)
     soup = BeautifulSoup(page, "html.parser")
-    posts = soup.find_all("li", class_="content-list__item content-list__item_post shortcuts_item")
+    posts = soup.find_all(
+        "li",
+        class_="content-list__item content-list__item_post shortcuts_item")
     for post in posts:
         try:
             time.sleep(1)
             user_d = post.find("header", class_="post__meta")
             date = user_d.find("span", class_="post__time").text
             user = user_d.find("span", class_="user-info__nickname").text
-            
+
             dict_['user'], dict_['date'] = user, date
             print(dict_)
             post_name = post.find("h2", class_="post__title").text
             print(post_name)
-            post_txt = post.find("div", class_="post__body post__body_crop").text
-            post_txt = ' '.join(re.sub("^\s+|\n|\r|\s+$", ' ', post_txt).split()[:-3])
+            post_txt = post.find(
+                "div", class_="post__body post__body_crop").text
+            post_txt = ' '.join(
+                re.sub("^\s+|\n|\r|\s+$", ' ', post_txt).split()[:-3])
             print(post_txt)
             dict_['post_name'], dict_['post_text'] = post_name, post_txt
             posts.append(dict_)
         except AttributeError:
             pass
     print(posts)
-        
+
 
 page(get_link())
